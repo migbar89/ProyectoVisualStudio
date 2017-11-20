3fd6d1e97dcc5a44ba574ad315abe7b3230ba9fd
migbar89 <33843965+migbar89@users.noreply.github.com>
2017-11-20 14:57:02 -0600

    11
    11

--- ProyectoGitHub/ProyectoGitHub/Form1.cs
+++ ProyectoGitHub/ProyectoGitHub/Form1.cs
@@ -15,5 +15,10 @@ namespace ProyectoGitHub
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Esto es el evento load
+        }
     }
 }
